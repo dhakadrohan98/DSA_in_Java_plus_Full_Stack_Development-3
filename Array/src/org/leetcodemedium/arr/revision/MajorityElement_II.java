cdc4da0b8114bdfa70651c1fd6920a32f57ccc1e
RohanDhakad <dhakadrohan98>
2025-03-31 21:05:56 +0530

    MajorityElement_II, NextPermutation & Reverse a linked list LC problems are solved

--- Array/src/org/leetcodemedium/arr/revision/MajorityElement_II.java
+++ Array/src/org/leetcodemedium/arr/revision/MajorityElement_II.java
@@ -1,27 +1,28 @@
 package org.leetcodemedium.arr.revision;
 
 import java.util.*;
-public class MajorityElement_II {
-	
-	//TC: O(n)
-    //SC: O(n)
-    public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> hmap = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int n = nums.length;
-        for(int num: nums) {
-            hmap.put(num, hmap.getOrDefault(num, 0) + 1);
-        }
 
-        for(int key: hmap.keySet()) {
-            int val = hmap.get(key);
-            if(val > (n / 3)) {
-                list.add(key);
-            }
-        }
-        return list;
-    }
-    
+public class MajorityElement_II {
+
+	// TC: O(n)
+	// SC: O(n)
+	public List<Integer> majorityElement(int[] nums) {
+		Map<Integer, Integer> hmap = new HashMap<>();
+		List<Integer> list = new ArrayList<>();
+		int n = nums.length;
+		for (int num : nums) {
+			hmap.put(num, hmap.getOrDefault(num, 0) + 1);
+		}
+
+		for (int key : hmap.keySet()) {
+			int val = hmap.get(key);
+			if (val > (n / 3)) {
+				list.add(key);
+			}
+		}
+		return list;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
---
+++ Nagarro_OA/src/org/oa/sample/SameFrequency.java
@@ -0,0 +1,10 @@
+package org.oa.sample;
+
+public class SameFrequency {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
