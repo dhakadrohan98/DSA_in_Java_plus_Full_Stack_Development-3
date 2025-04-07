6cea5dd91ac5f7d379afd64cf4b5a2ccb38a06ae
RohanDhakad <dhakadrohan98>
2025-04-07 18:46:10 +0530

    Built the Segment tree and FlatteningALinkedList hard leetcode problem is solved

---
+++ Segment_Tree/src/org/codeship/segment/tree/BuildSegmentTree.java
@@ -0,0 +1,54 @@
+package org.codeship.segment.tree;
+
+//TC: O(2N) = O(N)
+//SC: O(2N)
+public class BuildSegmentTree {
+
+	static int st[];
+
+	public static void buildSegmentTree(int i, int l, int r, int arr[]) {
+		// base case
+		if (l == r) {
+			st[i] = arr[l];
+			return;
+		}
+		int mid = (l + r) / 2;
+		//build the left subtree
+		buildSegmentTree(2 * i + 1, l, mid, arr);
+		//build the right subtree
+		buildSegmentTree(2 * i + 2, mid + 1, r, arr);
+		//store the sum of left & right child node into current node
+		st[i] = st[(2 * i) + 1] + st[(2 * i) + 2];
+	}
+
+	public static int[] construct(int[] arr) {
+		int n = arr.length;
+//		int height = (int)Math.log(n/2);
+//		int size = (int)Math.pow(2, height) - 1;
+		st = new int[2 * n - 1];
+
+		// build the segment tree
+		buildSegmentTree(0, 0, n - 1, arr);
+		return st;
+	}
+
+	public static void main(String[] args) {
+		int[] arr1 = { 3, 1, 2, 7, 5, 6, 3 };
+		construct(arr1);
+		// print the segment tree
+		System.out.print("s1 -> ");
+		for (int num : st) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+
+		System.out.print("s2 -> ");
+		int[] arr2 = { 3, 1, 2, 7 };
+		construct(arr2);
+		// print the segment tree
+		for (int num : st) {
+			System.out.print(num + " ");
+		}
+	}
+
+}
