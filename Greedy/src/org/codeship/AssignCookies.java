cae827bca65c209b2354597b6a9c859e5c304297
RohanDhakad <dhakadrohan98>
2025-04-13 11:59:20 +0530

    Jump game-I, II both problems, Lemonade change ans Assign cookies leetcode greedy problems are solved

--- Greedy/src/org/codeship/AssignCookies.java
+++ Greedy/src/org/codeship/AssignCookies.java
@@ -3,28 +3,28 @@ package org.codeship;
 import java.util.Arrays;
 
 public class AssignCookies {
-	
-	 //TC: O(nlogn) due to dual pivot quick sort
-    //SC: O(logn)
-    public int findContentChildren(int[] g, int[] s) {
-    Arrays.sort(g); // Sort greed factors
-    Arrays.sort(s); // Sort cookie sizes
-    
-    int l = 0; // Pointer for children (s)
-    int r = 0; // Pointer for greed (g)
-    
-    while (r < g.length && l < s.length) {
-        if (g[r] <= s[l]) {
-            // Cookie j satisfies child i
-            r++;
-        }
-        // Always move to next cookie
-        l++;
-    }
-    
-    return r; // Number of content children
-    }
-    
+
+	// TC: O(nlogn) due to dual pivot quick sort
+	// SC: O(logn)
+	public int findContentChildren(int[] g, int[] s) {
+		Arrays.sort(g); // Sort greed factors
+		Arrays.sort(s); // Sort cookie sizes
+
+		int l = 0; // Pointer for children (s)
+		int r = 0; // Pointer for greed (g)
+
+		while (r < g.length && l < s.length) {
+			if (g[r] <= s[l]) {
+				// Cookie j satisfies child i
+				r++;
+			}
+			// Always move to next cookie
+			l++;
+		}
+
+		return r; // Number of content children
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
