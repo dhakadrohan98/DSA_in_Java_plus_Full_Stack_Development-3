44a7451cb1acf1bae884424b36fca32edbb65e5c
RohanDhakad <dhakadrohan98>
2025-04-14 17:41:36 +0530

    TaskScheduler greedy problem is solved

--- Greedy/src/org/codeship/JumpGame.java
+++ Greedy/src/org/codeship/JumpGame.java
@@ -2,6 +2,19 @@ package org.codeship;
 
 public class JumpGame {
 	
+	//TC: O(n)
+    //SC: O(n)
+    public boolean canJump_CorrectApproach(int[] nums) {
+        int n = nums.length;
+        int reachable = 0;
+        for(int i = 0; i < n; i++) {
+            //current index is greater than maximum reachable indx till now
+            if(reachable < i) return false;
+            reachable = Math.max(reachable, i + nums[i]);
+        }
+        return true;
+    }
+	
 	// 69/174 test cases are passed
 	//TC: O(n)
 	//SC: O(1)
