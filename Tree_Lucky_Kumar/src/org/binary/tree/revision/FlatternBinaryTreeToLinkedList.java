b19b90a300f6709c2785c37bf3a348ef5f656630
RohanDhakad <dhakadrohan98>
2025-05-05 23:09:39 +0530

    FlatternBinaryTreeToLinkedList, PathSum_I, II, III & ConvertSortedArrayToBinarySearchTree leetcode problems are solved

--- Tree_Lucky_Kumar/src/org/binary/tree/revision/FlatternBinaryTreeToLinkedList.java
+++ Tree_Lucky_Kumar/src/org/binary/tree/revision/FlatternBinaryTreeToLinkedList.java
@@ -1,6 +1,19 @@
 package org.binary.tree.revision;
 
+//TC : O(n)
+//SC: O(n)
 public class FlatternBinaryTreeToLinkedList {
+
+	TreeNode prev = null;
+	public void flatten(TreeNode root) {
+		if (root == null)
+			return;
+		flatten(root.right);
+		flatten(root.left);
+		root.right = prev;
+		root.left = null;
+		prev = root;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
