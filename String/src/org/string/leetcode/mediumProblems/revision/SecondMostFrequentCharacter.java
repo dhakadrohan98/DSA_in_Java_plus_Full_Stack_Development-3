5b745d2816ee1cd40b70a58f090f2bf8719e7cc5
RohanDhakad <dhakadrohan98>
2025-05-06 13:18:13 +0530

    ValidateBinarySearchTree,SecondMostFrequentCharacter & SortCharactersByFrequency_HmapValueSorting leetcode problems are solved

---
+++ String/src/org/string/leetcode/mediumProblems/revision/SecondMostFrequentCharacter.java
@@ -0,0 +1,47 @@
+package org.string.leetcode.mediumProblems.revision;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SecondMostFrequentCharacter {
+
+	private static char findSecondMostFreqChar(String s) {
+		char res = ' ';
+		Map<Character, Integer> lhmap = new LinkedHashMap<>();
+		for (char ch : s.toCharArray()) {
+			if(ch == ' ') continue;
+			lhmap.put(ch, lhmap.getOrDefault(ch, 0) + 1);
+		}
+		System.out.println(lhmap);
+		
+		int largest = 0;
+		int secondLargest = 0;
+		for (char key : lhmap.keySet()) {
+			int val = lhmap.get(key);
+			if (largest < val) {
+				secondLargest = largest;
+				largest = val;
+			}
+
+			if (val < largest && secondLargest < val) {
+				secondLargest = val;
+			}
+		}
+
+		for (char key : lhmap.keySet()) {
+			if (lhmap.get(key) == secondLargest) {
+				res = key;
+				break;
+			}
+		}
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		String s = "programming languages";
+		char secondMostFreqChar = findSecondMostFreqChar(s);
+		System.out.println(secondMostFreqChar);
+	}
+
+}
---
+++ String/src/org/string/leetcode/mediumProblems/revision/SortCharactersByFrequency_HmapValueSorting.java
@@ -0,0 +1,36 @@
+package org.string.leetcode.mediumProblems.revision;
+
+import java.util.*;
+public class SortCharactersByFrequency_HmapValueSorting {
+
+	// TC: O(3n * logn)
+	// SC: O(2n)
+	public String frequencySort(String s) {
+		Map<Character, Integer> hmap = new HashMap<>();
+		for (char ch : s.toCharArray()) {
+			hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+		}
+
+		List<Map.Entry<Character, Integer>> list = new ArrayList<>(hmap.entrySet());
+		Collections.sort(list, (o1, o2) -> o2.getValue() - o1.getValue());
+		Map<Character, Integer> lhmap = new LinkedHashMap<>();
+		for (Map.Entry<Character, Integer> map : list) {
+			lhmap.put(map.getKey(), map.getValue());
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (char ch : lhmap.keySet()) {
+			int freq = lhmap.get(ch);
+			while (freq-- > 0) {
+				sb.append(ch);
+			}
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
