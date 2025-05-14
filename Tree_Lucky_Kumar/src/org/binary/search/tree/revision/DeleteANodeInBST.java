c8c4ff779da0a944eca16afd86ce1c6105b050ae
RohanDhakad <dhakadrohan98>
2025-05-15 00:20:05 +0530

    Decorator design pattern is implemented in coffee machine system

--- Tree_Lucky_Kumar/src/org/binary/search/tree/revision/DeleteANodeInBST.java
+++ Tree_Lucky_Kumar/src/org/binary/search/tree/revision/DeleteANodeInBST.java
@@ -1,57 +1,59 @@
 package org.binary.search.tree.revision;
 
 public class DeleteANodeInBST {
-	//TC: O(n)
-    //SC: O(< n) stack space in findLastRight method
-    //Steps
-    //Search the node & then delete it
-    public TreeNode deleteNode(TreeNode root, int key) {
-        //base case
-        if(root == null) return root;
-        if(root.val == key) {
-            return helper(root);
-        }
-        TreeNode dummy = root;
+	// TC: O(n)
+	// SC: O(< n) stack space in findLastRight method
+	// Steps
+	// Search the node & then delete it
+	public TreeNode deleteNode(TreeNode root, int key) {
+		// base case
+		if (root == null)
+			return root;
+		if (root.val == key) {
+			return helper(root);
+		}
+		TreeNode dummy = root;
 
-        while(root != null) {
-            if(key < root.val) {
-                if(root.left != null && root.left.val == key) {
-                    root.left = helper(root.left);
-                    break;
-                } else {
-                    root = root.left; 
-                }
-            } else {
-                if(root.right != null && root.right.val == key) {
-                    root.right = helper(root.right);
-                    break;
-                } else {
-                    root = root.right;
-                }
-            }
-        }
-        return dummy;
-   }
+		while (root != null) {
+			if (key < root.val) {
+				if (root.left != null && root.left.val == key) {
+					root.left = helper(root.left);
+					break;
+				} else {
+					root = root.left;
+				}
+			} else {
+				if (root.right != null && root.right.val == key) {
+					root.right = helper(root.right);
+					break;
+				} else {
+					root = root.right;
+				}
+			}
+		}
+		return dummy;
+	}
 
-   private TreeNode helper(TreeNode root) {
-        //edge cases
-        if(root.left == null) {
-            return root.right;
-        } else if(root.right == null) {
-            return root.left;
-        }
-        TreeNode rightChild = root.right;
-        TreeNode lastRight = lastRight = findLastRight(root.left);
-        lastRight.right = rightChild;
-        return root.left;
-   }
+	private TreeNode helper(TreeNode root) {
+		// edge cases
+		if (root.left == null) {
+			return root.right;
+		} else if (root.right == null) {
+			return root.left;
+		}
+		TreeNode rightChild = root.right;
+		TreeNode lastRight = lastRight = findLastRight(root.left);
+		lastRight.right = rightChild;
+		return root.left;
+	}
 
-   private TreeNode findLastRight(TreeNode root) {
-        //base case for recursion
-        //if current node is leaf node of right subtree then return it
-        if(root.right == null) return root;
-        TreeNode ansNode = findLastRight(root.right);
-        return ansNode;
-   }
+	private TreeNode findLastRight(TreeNode root) {
+		// base case for recursion
+		// if current node is leaf node of right subtree then return it
+		if (root.right == null)
+			return root;
+		TreeNode ansNode = findLastRight(root.right);
+		return ansNode;
+	}
 
 }
--- Tree_Lucky_Kumar/src/org/binary/search/tree/revision/KthSmallestElementInABST.java
+++ Tree_Lucky_Kumar/src/org/binary/search/tree/revision/KthSmallestElementInABST.java
@@ -4,15 +4,16 @@ import java.util.*;
 
 public class KthSmallestElementInABST {
 	public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        dfs(root, list);
-        return list.get(k-1);
-    }
+		List<Integer> list = new ArrayList<>();
+		dfs(root, list);
+		return list.get(k - 1);
+	}
 
-    private void dfs(TreeNode root, List<Integer> list) {
-        if(root == null) return;
-        dfs(root.left, list);
-        list.add(root.val);
-        dfs(root.right, list);
-    }
+	private void dfs(TreeNode root, List<Integer> list) {
+		if (root == null)
+			return;
+		dfs(root.left, list);
+		list.add(root.val);
+		dfs(root.right, list);
+	}
 }
