747fec899d3619e7bafc52cb9b0a6d9a683fe4ac
RohanDhakad <dhakadrohan98>
2025-12-07 23:05:06 +0530

    Stack & LinkedList problems are solved, its name is present in description of githu commit

--- PayU_DSA/src/payu/linkedlist/CycleDetection.java
+++ PayU_DSA/src/payu/linkedlist/CycleDetection.java
@@ -2,6 +2,25 @@ package payu.linkedlist;
 
 public class CycleDetection {
 
+	// TC: O(n)
+	// SC: O(n)
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != fast) {
+			slow = slow.next;
+			if (fast != null && fast.next != null) {
+				fast = fast.next.next;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- PayU_DSA/src/payu/linkedlist/MergeTwoSortedList.java
+++ PayU_DSA/src/payu/linkedlist/MergeTwoSortedList.java
@@ -1,54 +1,54 @@
 package payu.linkedlist;
 
 class ListNode {
-	
+
 	int val;
 	ListNode next;
-	
+
 	public ListNode(int val) {
 		this.val = val;
 	}
 }
 
 public class MergeTwoSortedList {
-	
-	//TC: O(n)
-    //SC: O(1)
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //edge case
-        if(list1 == null) {
-            return list2;
-        } else if(list2 == null) {
-            return list1;
-        } else if(list1 == null && list2 == null) {
-            return null;
-        }
-        ListNode dummyNode = new ListNode(-1);
-        ListNode temp = dummyNode;
-        ListNode t1 = list1;
-        ListNode t2 = list2;
-        while(t1 != null && t2 != null) {
-            if(t1.val <= t2.val) {
-                temp.next = t1;
-                temp = t1;
-                t1 = t1.next;
-            } else {
-                temp.next = t2;
-                temp = t2;
-                t2 = t2.next;
-            }
-        }
-        while(t1 != null) {
-            temp.next = t1;
-            temp = t1;
-            t1 = t1.next;
-        }
-        while(t2 != null) {
-            temp.next = t2;
-            temp = t2;
-            t2 = t2.next;
-        }
-        return dummyNode.next;
-    }
-    
+
+	// TC: O(n)
+	// SC: O(1)
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		// edge case
+		if (list1 == null) {
+			return list2;
+		} else if (list2 == null) {
+			return list1;
+		} else if (list1 == null && list2 == null) {
+			return null;
+		}
+		ListNode dummyNode = new ListNode(-1);
+		ListNode temp = dummyNode;
+		ListNode t1 = list1;
+		ListNode t2 = list2;
+		while (t1 != null && t2 != null) {
+			if (t1.val <= t2.val) {
+				temp.next = t1;
+				temp = t1;
+				t1 = t1.next;
+			} else {
+				temp.next = t2;
+				temp = t2;
+				t2 = t2.next;
+			}
+		}
+		while (t1 != null) {
+			temp.next = t1;
+			temp = t1;
+			t1 = t1.next;
+		}
+		while (t2 != null) {
+			temp.next = t2;
+			temp = t2;
+			t2 = t2.next;
+		}
+		return dummyNode.next;
+	}
+
 }
