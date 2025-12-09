ad15147549f8eb57018e450e930686f595470b3b
RohanDhakad <dhakadrohan98>
2025-12-09 22:29:20 +0530

    RemoveNthEndFromEndOfList, approach for AddTwoNumbers problem is being written there

---
+++ PayU_DSA/src/payu/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,21 @@
+package payu.linkedlist;
+
+public class AddTwoNumbers {
+
+	/**
+	 * 1. Traverse from starting in both LL
+	 * Add correspond digit one by one, store resultant digit into one Arraylist
+	 *    - sum % M
+	 *    if(sum >= 10) {
+	 *    	sum / M
+	 *    }
+	 * 3. Then made nodes from ArrayList from i=0 to n-1.
+	 *    
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
